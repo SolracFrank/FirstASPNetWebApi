2ee203a07ff083dff1f573d1ee7316fc78e80036
Francisco Herrera <francisco.herrrera@proan.com>
2023-08-22 14:04:28 -0600

    Cambio de herencia de ApplicationDBContext para usar Identity en DATA

--- APIPeliculas/APIPeliculas/Data/ApplicationDBContext.cs
+++ APIPeliculas/APIPeliculas/Data/ApplicationDBContext.cs
@@ -1,18 +1,25 @@
 ﻿using APIPeliculas.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace APIPeliculas.Data
 {
-    public class ApplicationDBContext : DbContext
+    public class ApplicationDBContext : IdentityDbContext<AppUsuario>
     {
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) :base(options)
         {
                
            
         }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
         //Agregar los modelos aquí
         public DbSet<Categoria> categorias { get; set; }
         public DbSet<Pelicula> peliculas { get; set; }
         public DbSet<Usuario> usuarios { get; set; }
+        public DbSet<AppUsuario> appUsuarios { get; set; }
     }
 }
